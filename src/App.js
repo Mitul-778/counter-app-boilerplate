69f5b04f865ca34a8a14d4df2b5c5b3ffa22a735
Mitul Asodariya <70693181+Mitul-778@users.noreply.github.com>
2022-06-14 19:18:14 +0530

    all cases are passed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import { useState } from 'react'
 
 function App() {
+  const[X,setCount]= useState(0)
+  const handleChange=(n)=>{
+
+      setCount(X+n)
+  }
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{X}</h2>
+      <button data-testid="counter-decrement-button" disabled={X<=0 ? true : false} onClick={()=>handleChange(-1)}>-</button>
+      <button data-testid="counter-increment-button" onClick={()=>handleChange(1)}>+</button>
     </div>
   );
 }
